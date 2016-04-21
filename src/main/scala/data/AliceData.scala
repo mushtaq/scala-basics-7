d6da3a9d48ac9ff77d6bd7f50bc9be11fbe0255f
Mushtaq Ahmed <mushtaq.a@gmail.com>
2016-04-21 16:32:36 +0530

    regex

--- src/main/scala/data/AliceData.scala
+++ src/main/scala/data/AliceData.scala
@@ -5,4 +5,7 @@ import scala.io.Source
 object AliceData {
   val bookText = Source.fromFile("/Users/mushtaq/projects/workshops/scala-basics-7/src/main/resources/aliceInWonderland.txt").mkString
   val stopWordText = Source.fromFile("/Users/mushtaq/projects/workshops/scala-basics-7/src/main/resources/stopWords.txt").mkString
+
+  val bookRegex = """[\s|:|.|,|"]+"""
+  val stopWordRegex = "\\s+"
 }
